08ac13734d58357282c9a7b46a06cb630ad5904d
avmhdt <josessc.js@gmail.com>
2021-07-30 14:41:23 -0300

    commit inicial - comecei funcao lotterySched

---
+++ lottery.c
@@ -0,0 +1,78 @@
+/*
+*  lottery.c - Implementacao do algoritmo Lottery Scheduling e sua API
+*
+*  Autores: SUPER_PROGRAMADORES_C
+*  Projeto: Trabalho Pratico I - Sistemas Operacionais
+*  Organizacao: Universidade Federal de Juiz de Fora
+*  Departamento: Dep. Ciencia da Computacao
+*
+*
+*/
+
+#include "lottery.h"
+#include <stdio.h>
+#include <string.h>
+
+//Nome unico do algoritmo. Deve ter 4 caracteres.
+const char lottName[]="LOTT";
+
+//=====Funcoes Auxiliares=====
+
+int nTickets = 0;
+
+// Retorna inteiro aleatório entre [a, b)
+int getRandom(a, b) {
+    return a + rand() % b;
+}
+
+
+//=====Funcoes da API=====
+
+//Funcao chamada pela inicializacao do S.O. para a incializacao do escalonador
+//conforme o algoritmo Lottery Scheduling
+//Deve envolver a inicializacao de possiveis parametros gerais
+//Deve envolver o registro do algoritmo junto ao escalonador
+void lottInitSchedInfo() {
+	//...
+}
+
+//Inicializa os parametros de escalonamento de um processo p, chamada
+//normalmente quando o processo e' associado ao slot de Lottery
+void lottInitSchedParams(Process *p, void *params) {
+	//...
+	processSetSchedParams(p, params);
+}
+
+//Retorna o proximo processo a obter a CPU, conforme o algortimo Lottery
+Process* lottSchedule(Process *plist) {
+	//...
+    int counter = 0;
+    int winner = getRandom(0, nTickets);
+    Process *current = plist;
+    void *currentParams;
+    while(current) {
+        currentParams = processGetSchedParams(current);
+
+        counter += ->num_tickets;
+        if(counter > winner)
+            break;
+        current = current->next;
+    }
+//	return NULL;
+    return current;
+}
+
+//Libera os parametros de escalonamento de um processo p, chamada
+//normalmente quando o processo e' desassociado do slot de Lottery
+//Retorna o numero do slot ao qual o processo estava associado
+int lottReleaseParams(Process *p) {
+	//...
+	return 0;
+}
+
+//Transfere certo numero de tickets do processo src para o processo dst.
+//Retorna o numero de tickets efetivamente transfeirdos (pode ser menos)
+int lottTransferTickets(Process *src, Process *dst, int tickets) {
+	//...
+	return 0;
+}
